27497ba83f2808204acfb16cbbd7f282b2a063b4
azimatemirova <shoxruxravshanov07@gmail.com>
2022-11-17 18:27:32 +0300

    website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]')
+      tabContents = document.querySelectorAll('[data-content]')
 
-
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.taraget)
+        
+        tabContents.forEach(tc =>{
+            tc.classList.remove('filtres__active')
+        })
+    })
+})
 /*=============== DARK LIGHT THEME ===============*/
 
 
